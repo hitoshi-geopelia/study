fb9066293077775d5457051d9e197bb6592ea0b9
hitoshi-geopelia <hitoshi.geopelia@gmail.com>
2020-06-02 12:29:00 +0900

    Update driver (function call code)

--- int2abc.py
+++ int2abc.py
@@ -11,7 +11,6 @@ def int2abc(in1):
   abc = ''
   for col in range(len(blist) - 1, -1, -1):
     if 1 < col:
-      # print('  {} {} {}'.format(in1, BASE ** col, blist))
       a = (in1 - blist[col - 2]) // (BASE ** col)
     else:
       a = in1 // (BASE ** col)
@@ -23,5 +22,17 @@ def int2abc(in1):
   return abc
 
 # print(int2abc(int(args[ 1 ])))
-for i in range(0, 121):
-  print('★ {} = {}'.format(('  ' + str(i))[-2:], int2abc(i)))
+for i in range(0, 800, 13):
+  print('{}'.format(('    ' + int2abc(i))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 1))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 2))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 3))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 4))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 5))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 6))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 7))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 8))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 9))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 10))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 11))[-4:]) +
+        '{}'.format(('    ' + int2abc(i + 12))[-4:]))
